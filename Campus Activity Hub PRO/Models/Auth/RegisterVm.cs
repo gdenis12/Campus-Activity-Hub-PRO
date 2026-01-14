1e68b35b508428e7cec0a2a2a6654e72deea8ba8
gdenis12 <vitalij.poleshhuk.04@gmail.com>
2026-01-14 22:59:23 +0200

    Add new Model  | Add Controller | Add new Pages | Update Layout

---
+++ Campus Activity Hub PRO/Models/Auth/RegisterVm.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Campus_Activity_Hub_PRO.Models.Auth
+{
+    public class RegisterVm
+    {
+        [Required]
+        [StringLength(60)]
+        public string Name { get; set; } = "";
+
+        [Required, EmailAddress]
+        public string Email { get; set; } = "";
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = "";
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; } = "";
+
+        [Required]
+        public string Role { get; set; } = "Student"; 
+    }
+}
