d8cf591107eeafed38f1fdea61fc670c3fb65a0c
gdenis12 <vitalij.poleshhuk.04@gmail.com>
2026-01-14 21:54:35 +0200

    Добавьте файлы проекта.

---
+++ Campus Activity Hub PRO/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Campus_Activity_Hub_PRO.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
